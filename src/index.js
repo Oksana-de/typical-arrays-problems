934ef92f0a3cd1013483c3aea09bd1c5e4311156
Oksana-de <77386093+Oksana-de@users.noreply.github.com>
2021-03-15 19:19:47 +0100

    feat: add some code

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
+exports.min = function min(array) {
+    if ((typeof array !== 'undefined') || (typeof array !== 'NaN')) {
+        if (array.length > 0) {
+            var solution = Math.min.apply(0, array);
+        } else {
+            solution = 0;
+        }
+    } else {
+        solution = 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
+    return solution;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if ((typeof array !== 'undefined') || (typeof array !== 'NaN')) {
+        if (array.length > 0) {
+            var solution = Math.max.apply(0, array);
+        } else {
+            solution = 0;
+        }
+    } else {
+        solution = 0;
+    }
+
+    return solution;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if ((typeof array !== 'undefined') || (typeof array !== 'NaN')) {
+        if (array.length > 0) {
+            var summ = 0;
+            for (var i = 0; i < array.length; i++) {
+                summ = summ + array[i];
+            }
+            solution = summ / array.length;
+        } else {
+            solution = 0;
+        }
+    } else {
+        solution = 0;
+    }
+    return solution;
 }
