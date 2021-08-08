5d9ddf92dd575bfcafa4c241d58427bccf94ce7a
Oksana Vovere <oksana.vovere@gmail.com>
2021-08-08 13:22:58 +0200

    feat: final solution for task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,44 +1,11 @@
-exports.min = function min(array) {
-    if ((typeof array !== 'undefined') || (typeof array !== 'NaN')) {
-        if (array.length > 0) {
-            var solution = Math.min.apply(0, array);
-        } else {
-            solution = 0;
-        }
-    } else {
-        solution = 0;
-    }
-
-    return solution;
-}
+exports.min = function min(array) {    
+    return (array == undefined || array.length === 0)?0:Math.min(...array)
+  }
 
 exports.max = function max(array) {
-    if ((typeof array !== 'undefined') || (typeof array !== 'NaN')) {
-        if (array.length > 0) {
-            var solution = Math.max.apply(0, array);
-        } else {
-            solution = 0;
-        }
-    } else {
-        solution = 0;
-    }
-
-    return solution;
-}
+    return (array == undefined || array.length === 0)?0:Math.max(...array)
+  }
 
 exports.avg = function avg(array) {
-    if ((typeof array !== 'undefined') || (typeof array !== 'NaN')) {
-        if (array.length > 0) {
-            var summ = 0;
-            for (var i = 0; i < array.length; i++) {
-                summ = summ + array[i];
-            }
-            solution = summ / array.length;
-        } else {
-            solution = 0;
-        }
-    } else {
-        solution = 0;
-    }
-    return solution;
-}
+    return (array == undefined || array.length === 0)?0:[...array].reduce((acc, i) => acc += i, 0) / array.length
+  }
